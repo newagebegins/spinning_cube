5b56fe21a7e0ef69ca8383a133bec6e7d46acc1d
newagebegins <newagebegins@gmail.com>
2016-04-13 20:34:42 +1000

    add shell.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 if not exist build mkdir build
 pushd build
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 cl /MTd /nologo /fp:fast /Gm- /GR- /EHa- /Zo /Od /Oi /WX /W4 /FC /Z7 /wd4189 /wd4505 /wd4201 /wd4100 ..\cg.cpp /link /INCREMENTAL:NO /OPT:REF user32.lib gdi32.lib
 popd
